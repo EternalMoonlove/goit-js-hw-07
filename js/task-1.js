50676a0f01fa81f94db20847c76830db58bd14e0
Denis <denisklepach060203@icloud.com>
2025-04-26 12:56:28 +0300

    update

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+// const categoryItems = document.querySelector("#categories");
+// const items = categoryItems.querySelectorAll(".item");
+
+// console.log(`Number of categories: ${items.length}`);
+
+// items.forEach((item) => {
+//   const header = item.querySelector(".list-header");
+//   const headerText = header.textContent;
+//   const itemsCount = item.querySelectorAll("ul li").length;
+
+//   console.log(`Category: ${headerText}`);
+//   console.log(`Elements: ${itemsCount}`);
+// });
